277bcef3a995fc2a85958258fab15c700ca5e089
Robin Kaiser <web@r-k.mx>
2021-11-05 21:51:30 +0100

    auth v2 middleware implementiert

--- middleware.go
+++ middleware.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
 
 func authV1(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +26,29 @@ func authV1(next http.Handler) http.Handler {
 
 func authV2(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		header := r.Header.Get("Authorization")
+		if header == "" {
+			http.Error(w, "Authentication failed (Temp Key missing)", http.StatusBadRequest)
+			return
+		}
+
+		parts := strings.Split(header, " ")
+		if len(parts) != 2 {
+			http.Error(w, "Authentication failed (malformed Authorization header)", http.StatusBadRequest)
+			return
+		}
+
+		apiKey, err := base64.StdEncoding.DecodeString(parts[1])
+		if err != nil {
+			http.Error(w, "Authentication failed (base64 decode failed)", http.StatusInternalServerError)
+			return
+		}
+
+		if !authenticateV1(string(apiKey)) {
+			http.Error(w, "Authentication failed (API Key invalid)", http.StatusUnauthorized)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
